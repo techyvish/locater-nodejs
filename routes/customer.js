94ce6d1f76b0404380e963ba537cbbb3ef43b301
Vishal <vishal.x.patel@nab.com.au>
2014-12-14 19:14:39 +1100

    Minor change

--- routes/customer.js
+++ routes/customer.js
@@ -4,17 +4,23 @@
 
 exports.list = function (req, res) {
     req.getConnection(function (err,connection) {
-        var query = connection.query('SELECT * FROM customers',function(err,rows){
-            if ( err )
-            {
-                console.log("Error selecting");
-                res.status(500);
-                res.send('No data found');
-            }
-            else {
-                res.status(200);
-                res.send(JSON.stringify(rows));
-            }
-        });
+        if ( typeof connection != "undefined" ) {
+            var query = connection.query('SELECT * FROM customers', function (err, rows) {
+                if (err) {
+                    console.log("Error selecting");
+                    res.status(500);
+                    res.send('No data found');
+                }
+                else {
+                    res.status(200);
+                    res.send(JSON.stringify(rows));
+                }
+            });
+        }
+        else
+        {
+            res.status(500);
+            res.send('No db connection found');
+        }
     });
 };
